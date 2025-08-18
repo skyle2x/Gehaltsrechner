140ae5f15ade2aa4040b15aeedc9334fe6b02f07
skyle2x <137298502+skyle2x@users.noreply.github.com>
2025-08-18 14:55:41 +0200

    Add files via upload

---
+++ Gehaltsrechner.py
@@ -0,0 +1,10 @@
+Studenlohn =input("Bitte dein Stundenlohn eingeben:")
+Tag= 8* int(Studenlohn)
+monat= Tag * 20
+year=monat * 12
+
+print("Dein gehalt beträgt " + str(Studenlohn) + "€")
+print("Du verdients " + str(Tag) + "€" + " pro tag")
+print("Du verdients " + str(monat) + "€ pro monat")
+print("Du verdients " + str(year) + "€ pro Jahr")
+input("Drücke eine beliebige ")
